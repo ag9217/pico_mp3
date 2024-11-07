db60a9a7681bef9de8e469b329f4ee27837d77fa
Armand <grigorianarmand@gmail.com>
2024-11-07 22:15:53 +0000

    mixture of pico-playground and tinysound v2, still not working

--- lib/fs/fat.c
+++ lib/fs/fat.c
@@ -1,9 +1,76 @@
 #include "fat.h"
+#include "hardware/clocks.h"
+#include "audio_i2s.pio.h"
+#include "pico/audio_i2s.h"
+#include "hardware/dma.h"
+#include <hardware/pio.h>
+#include <hardware/structs/io_bank0.h>
+#include <hardware/sync.h>
 
 struct fat_block_device fs = {
     .init = block_fs_init,
     .open = open_file
 };
+
+#define SAMPLES_PER_BUFFER 512
+int16_t audio_buffer[2][SAMPLES_PER_BUFFER];
+uint8_t play_buffer_index = 0;
+static bool buffer_empty = false;
+bool playing = false;
+
+static void Audio_UpdateFrequency(uint32_t sample_freq)
+{
+    uint32_t system_clock_frequency = clock_get_hz(clk_sys);
+    uint32_t divider = system_clock_frequency * 4 / sample_freq;
+    pio_sm_set_clkdiv_int_frac(pio0, 0, divider >> 8u, divider & 0xffu);
+}
+
+void __isr __time_critical_func(Audio_DMACallback)(void)
+{
+    if (dma_irqn_get_channel_status(0, 0))
+    {
+        dma_irqn_acknowledge_channel(0, 0);
+
+        play_buffer_index = play_buffer_index ? 0 : 1;
+        dma_channel_transfer_from_buffer_now(0, audio_buffer[play_buffer_index], SAMPLES_PER_BUFFER / 4);
+        buffer_empty = true;
+    }
+}
+
+struct audio_buffer_pool *init_audio() {
+
+    static audio_format_t audio_format = {
+            .format = AUDIO_BUFFER_FORMAT_PCM_S16,
+            .sample_freq = 22050,
+            .channel_count = 1,
+    };
+
+    static struct audio_buffer_format producer_format = {
+            .format = &audio_format,
+            .sample_stride = 8
+    };
+
+    struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 3,
+                                                                      SAMPLES_PER_BUFFER); // todo correct size
+    bool __unused ok;
+    const struct audio_format *output_format;
+    struct audio_i2s_config config = {
+            .data_pin = PICO_AUDIO_I2S_DATA_PIN,
+            .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+            .dma_channel = 0,
+            .pio_sm = 0,
+    };
+
+    output_format = audio_i2s_setup(&audio_format, &config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    ok = audio_i2s_connect(producer_pool);
+    assert(ok);
+    audio_i2s_set_enabled(true);
+    return producer_pool;
+}
 
 static int block_fs_init() {
     // reading master boot record
@@ -81,6 +148,48 @@ static int open_file(char filename[]) {
     uint32_t bytes_read = 0;
     bool done_reading = false;
 
+    // audio
+    //struct audio_buffer_pool *ap = init_audio();
+    //struct audio_buffer *buffer = take_audio_buffer(ap, true);
+    //int16_t *samples = (int16_t *) buffer->buffer->bytes;
+    // setup I2S pins
+    gpio_set_function(PICO_AUDIO_I2S_DATA_PIN, GPIO_FUNC_PIO0);
+    gpio_set_function(PICO_AUDIO_I2S_CLOCK_PIN_BASE, GPIO_FUNC_PIO0);
+
+    // setup PIO0 state machine 0 with I2S program
+    pio_sm_claim(pio0, 0);
+    uint offset = pio_add_program(pio0, &audio_i2s_program);
+    audio_i2s_program_init(pio0, 0, offset, PICO_AUDIO_I2S_DATA_PIN, PICO_AUDIO_I2S_CLOCK_PIN_BASE);
+    __mem_fence_release();
+
+    // setup DMA channel
+    dma_channel_claim(0);
+
+    dma_channel_config dma_config = dma_channel_get_default_config(0);
+    channel_config_set_read_increment(&dma_config, true);
+    channel_config_set_dreq(&dma_config, DREQ_PIO0_TX0 + 0);
+    channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_32);
+
+    dma_channel_configure(0,
+                          &dma_config,
+                          &pio0->txf[0],                 // dest:  TX buffer of PIO0, statemachine 0
+                          NULL,                                 // src:   will be defined later
+                          SAMPLES_PER_BUFFER / 4,                      // count: half of the buffer (counted in 32bit words)
+                          false);                               // don't trigger immediately
+
+    irq_add_shared_handler(DMA_IRQ_0, Audio_DMACallback, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+    dma_irqn_set_channel_enabled(0, 0, true);
+    irq_set_enabled(DMA_IRQ_0, true);
+
+    // Set correct I2S frequency
+    Audio_UpdateFrequency(22050);
+    __mem_fence_release();
+
+    // Connect buffer
+    play_buffer_index = 0;
+    buffer_empty = true;
+    playing = true;
+
     ret = search_for_file(filename);
     if (ret < 0) {
         Log(LOG_ERROR, "Could not find file", -1);
@@ -93,19 +202,47 @@ static int open_file(char filename[]) {
     printf("First cluster: %x\n", fs.files[ret].first_cluster);
     printf("File size: %d\n", fs.files[ret].filesize);
 
-    // start reading file
+    // reading file header
     uint32_t cluster = fs.files[ret].first_cluster;
+    fs.blk_dev->read_block(cluster_to_lba(cluster));
+
+    printf("File size: %d\n", big_to_small_endian32(&(fs.blk_dev->in_buf[4])));
+    printf("Length of format data: %d\n", big_to_small_endian32(&(fs.blk_dev->in_buf[16])));
+    printf("Type of format: %d\n", big_to_small_endian16(&(fs.blk_dev->in_buf[20])));
+    printf("Channels: %d\n", big_to_small_endian16(&(fs.blk_dev->in_buf[22])));
+    printf("Sample rate: %d\n", big_to_small_endian32(&(fs.blk_dev->in_buf[24])));
+    printf("Sample rate * BitsPerSample * Channels / 8: %d\n", big_to_small_endian32(&(fs.blk_dev->in_buf[28])));
+    printf("BitsPerSample * Channels / 8: %d\n", big_to_small_endian16(&(fs.blk_dev->in_buf[32])));
+    printf("BitsPerSample: %d\n", big_to_small_endian16(&(fs.blk_dev->in_buf[34])));
+    printf("Data section size: %d\n", big_to_small_endian32(&(fs.blk_dev->in_buf[40])));
+    pio_sm_set_enabled(pio0, 0, true);
+
+    // start reading file
+    //uint32_t cluster = fs.files[ret].first_cluster;
     fs.fat_sector_offset = cluster / FAT32_ENTRIES_PER_FAT;
     fs.blk_dev->read_block(fs.fat_begin_lba + fs.fat_sector_offset);
     do {
         for (int sector_offset = 0; sector_offset < fs.sectors_per_cluster; sector_offset++) {
             fs.blk_dev->read_block(cluster_to_lba(cluster) + sector_offset);
+            memcpy((uint8_t*) audio_buffer, fs.blk_dev->in_buf, SAMPLES_PER_BUFFER);
             bytes_read += 512;
 
             if (bytes_read >= fs.files[ret].filesize) {
                 done_reading = true;
                 break;
             }
+
+            dma_channel_transfer_from_buffer_now(0, audio_buffer[play_buffer_index], SAMPLES_PER_BUFFER / 4);
+            pio_sm_set_enabled(pio0, 0, true);
+
+
+            //for (uint i = 0; i < buffer->max_sample_count; i+=2) {
+            //    samples[i] = (fs.blk_dev->in_buf[i]) | (fs.blk_dev->in_buf[i+1] << 8);
+            //}
+
+            //buffer->sample_count = buffer->max_sample_count;
+            //give_audio_buffer(ap, buffer);
+
         }
 
         // check fat
